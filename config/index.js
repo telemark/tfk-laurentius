dedc9f3e7315fd21828de3319073f4b513aa62af
Jonas Enge <jonas.enge@t-fk.no>
2016-02-22 14:45:57 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,11 +4,9 @@ var config = {
   JWT_KEY: process.env.JWT_KEY || 'NeverShareYourSecret',
   CALLBACK_STATUS_MESSAGE: process.env.CALLBACK_STATUS_MESSAGE || 'Varselbrev produsert',
   CALLBACK_STATUS_URL: process.env.CALLBACK_STATUS_URL || false,
-  JOB_DIRECTORY_PATH: process.env.JOB_DIRECTORY_PATH || 'test/data/jobs',
-  DISTRIBUTION_DIRECTORY_PATH: process.env.DISTRIBUTION_DIRECTORY_PATH || 'test/data/distribution',
-  ARCHIVE_DIRECTORY_PATH: process.env.ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
-  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/data/done',
-  ERROR_DIRECTORY_PATH: process.env.ERROR_DIRECTORY_PATH || 'test/data/errors',
+  ARCHIVE_JOB_DIRECTORY_PATH: process.env.ARCHIVE_JOB_DIRECTORY_PATH || 'test/data/archive/jobs',
+  ARCHIVE_DONE_DIRECTORY_PATH: process.env.ARCHIVE_DONE_DIRECTORY_PATH || 'test/data/archive/done',
+  ARCHIVE_ERROR_DIRECTORY_PATH: process.env.ARCHIVE_ERROR_DIRECTORY_PATH || 'test/data/archive/errors',
   p360: {
     user: 'domain/username', // username
     password: 'password', // passord
