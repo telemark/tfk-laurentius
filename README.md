fe373a2f763db383ac6334011b4c145655ca247e
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-03-02 13:27:00 +0100

    Adds Docker. Adds namespace for ENV

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 ## Innhold
 - [Arbeidsflyt](docs/workflow.md)
 - [Regler](docs/rules.md)
+
+## Docker
+Build
+
+```sh
+$ docker build -t tfk-laurentius .
+```
+
+### Usage
+```sh
+$ docker run --env-file=docker.env --volume=/test/data/jobs:/src/test/data/jobs --rm tfk-laurentius
+```
+
+This will start a container. Do the job. Stop the container and remove it.
--- config/index.js
+++ config/index.js
@@ -1,16 +1,15 @@
 'use strict'
 
 var config = {
-  JWT_KEY: process.env.JWT_KEY || 'NeverShareYourSecret',
-  CALLBACK_STATUS_MESSAGE: process.env.CALLBACK_STATUS_MESSAGE || 'Varselbrev produsert',
-  CALLBACK_STATUS_URL: process.env.CALLBACK_STATUS_URL || false,
-  ARCHIVE_JOB_DIRECTORY_PATH: process.env.ARCHIVE_JOB_DIRECTORY_PATH || 'test/data/archive/jobs',
-  ARCHIVE_DONE_DIRECTORY_PATH: process.env.ARCHIVE_DONE_DIRECTORY_PATH || 'test/data/archive/done',
-  ARCHIVE_ERROR_DIRECTORY_PATH: process.env.ARCHIVE_ERROR_DIRECTORY_PATH || 'test/data/archive/errors',
+  JWT_KEY: process.env.TFK_LAURENTIUS_JWT_KEY || 'NeverShareYourSecret',
+  CALLBACK_STATUS_MESSAGE: process.env.TFK_LAURENTIUS_CALLBACK_STATUS_MESSAGE || 'Varselbrev produsert',
+  ARCHIVE_JOB_DIRECTORY_PATH: process.env.TFK_LAURENTIUS_ARCHIVE_JOB_DIRECTORY_PATH || 'test/data/archive/jobs',
+  ARCHIVE_DONE_DIRECTORY_PATH: process.env.TFK_LAURENTIUS_ARCHIVE_DONE_DIRECTORY_PATH || 'test/data/archive/done',
+  ARCHIVE_ERROR_DIRECTORY_PATH: process.env.TFK_LAURENTIUS_.ARCHIVE_ERROR_DIRECTORY_PATH || 'test/data/archive/errors',
   p360: {
-    user: process.env.P360WS_USER || 'domain/username', // username
-    password: process.env.P360WS_PASSWORD || 'password', // passord
-    baseUrl: process.env.P360WS_BASEURL || 'http://tfk-fh-siweb01t.login.top.no:8088/SI.WS.Core/SIF/',
+    user: process.env.TFK_LAURENTIUS_P360WS_USER || 'domain/username', // username
+    password: process.env.TFK_LAURENTIUS_P360WS_PASSWORD || 'password', // passord
+    baseUrl: process.env.TFK_LAURENTIUS_P360WS_BASEURL || 'http://tfk-fh-siweb01t.login.top.no:8088/SI.WS.Core/SIF/',
     options: {
       ignoredNamespaces: true
     }
